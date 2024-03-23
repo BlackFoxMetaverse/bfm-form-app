d045ccac8db0b254113f2bab93d2bbe583aed1f8
Raunak Pandey <praunak82@gmail.com>
2024-03-23 18:21:48 +0530

    google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
 import Navbar from "./componenets/Navbar";
 import Home from "./componenets/screens/Home";
+import ReactGA from "react-ga4";
 
 function App() {
+  useEffect(() => {
+    console.log("Initializing Google Analytics...");
+
+    // Initialize Google Analytics
+    ReactGA.initialize("G-L4LNX64KCQ");
+
+    // Track the initial pageview
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.pathname,
+      title: document.title,
+    });
+
+    console.log("Google Analytics initialized. Tracking initial pageview.");
+  }, []);
+
   return (
     <main>
       <Navbar />
